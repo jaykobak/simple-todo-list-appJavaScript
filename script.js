e9796e46cd83f92a46123775a0a7ec7692af51de
Jacob <akintolajacob75@gmail.com>
2026-04-06 00:00:16 +0100

    feat: make the displayAllItems function dynamic to the user input

--- script.js
+++ script.js
@@ -12,19 +12,25 @@ const addItem = () => {
     else {
         allLists.push(input) // Store user input in array
 
-        listGroup.innerHTML = ''
+        document.getElementById('userInput').value = '' // Clear input box
 
-        for (i = 0; i < allLists.length; i++) {
-            listGroup.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
+        displayAllLists()
+    }
+}
+
+function displayAllLists() {
+    listGroup.innerHTML = ''
+
+    for (i = 0; i < allLists.length; i++) {
+        listGroup.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="todo1" />
-                    <label class="form-check-label" for="todo1">Learn arrays</label>
+                    <input class="form-check-input" type="checkbox" id="todo${i + 1}" />
+                    <label class="form-check-label" for="todo${i + 1}">${allLists[i]}</label>
                 </div>
                 <div>
                     <button class="btn btn-sm btn-outline-secondary" type="button">Edit</button>
                     <button class="btn btn-sm btn-outline-danger" type="button">Delete</button>
                 </div>
             </li>`
-        }
     }
 }
