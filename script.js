b885bab3163c4e259637234da78398c5a7aa8a6c
Jacob <akintolajacob75@gmail.com>
2026-04-05 23:32:33 +0100

    feat: add input validation for adding items to the todo list

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ const allLists = []
 const addItem = () => {
     // Get users input
     const input = document.getElementById('userInput').value
-    console.log(input)
+    
+    // Input Validation
+    if (input.trim() == '') {
+        alert("Please enter a valid input!")
+    }
+
+    else {
+        // Store user input in array
+        allLists.push(input)
+
+        emptyState.innerHTML = ''
+    }
 }
