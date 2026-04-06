134fa0fdfd27f14c33c7de3e17fd436ffe3a666f
Jacob <akintolajacob75@gmail.com>
2026-04-06 15:59:48 +0100

    feat: update display logic for empty todo list state

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const allLists = []
+// Display all items to the screen
+displayAllLists()
 
 const addItem = () => {
     // Get users input
@@ -32,10 +34,18 @@ const deleteItem = (index) => {
 }
 
 function displayAllLists() {
-    listGroup.innerHTML = ''
+    if (allLists.length == 0) {
+        listGroup.innerHTML = `<li class="list-group-item empty-state text-center py-4">
+            <i class="bi bi-journal-check empty-icon" aria-hidden="true"></i>
+            <p class="mb-0 mt-2 text-muted">Your list will be shown here</p>
+        </li>`
+    }
 
-    for (i = 0; i < allLists.length; i++) {
-        listGroup.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
+    else {
+        listGroup.innerHTML = ''
+
+        for (i = 0; i < allLists.length; i++) {
+            listGroup.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" id="todo${i + 1}" />
                     <label class="form-check-label" for="todo${i + 1}">${allLists[i]}</label>
@@ -45,5 +55,6 @@ function displayAllLists() {
                     <button onclick="deleteItem(${i})" class="btn btn-sm btn-outline-danger" type="button">Delete</button>
                 </div>
             </li>`
+        }
     }
 }
